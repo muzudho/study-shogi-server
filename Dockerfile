3b42a13df3088fe975e4922eebbafc0d9231e93e
むずでょ <muzudho1@gmail.com>
2021-05-18 23:58:55 +0900

    テストのログと、サーバーのログを分けようとしたが、意味ないようだぜ（＾～＾）

--- Dockerfile
+++ Dockerfile
@@ -14,10 +14,13 @@ ENV EVENT local
 ENV PORT 4081
 ENV MAX_IDENTIFIER 32
 
-# １階層目にログ・ディレクトリを置きます
+# １階層目には、テスト用のログ・ディレクトリを置きます（場所固定）
 WORKDIR /logs
 
-# １階層目にshogi-serverディレクトリを置きます
+# ２階層目には、サーバー用のログ・ディレクトリを置くように合わせます
+WORKDIR /shogi-server/logs
+
+# １階層目に戻ります
 WORKDIR /shogi-server
 
 RUN gem install rgl
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,10 @@
 
 # Please use LF for line breaks.
 
+# テストで使うlogsディレクトリは１階層目に置くようだぜ（＾～＾）空の shogi-server.log ファイルも作っておいてしまおう（＾～＾）
+touch /logs/shogi-server.log
+
 cd /shogi-server
 
-# logsディレクトリは１階層目に置くようだぜ（＾～＾）絶対パスで指定するぜ（＾～＾）
-ruby shogi-server --daemon /logs --max-identifier $MAX_IDENTIFIER $EVENT $PORT & tail -F /logs/shogi-server.log
+# サーバーが使うlogsディレクトリは、テストで使うlogsディレクトリとは別の場所にしよう。ややこしいんで絶対パスで指定するぜ（＾～＾）
+ruby shogi-server --daemon /shogi-server/logs --max-identifier $MAX_IDENTIFIER $EVENT $PORT & tail -F /shogi-server/logs/shogi-server.log
